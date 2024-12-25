8137cbc2fef35cd8e7b1937e6d4b6eb17f690930
dogeystamp <dogeystamp@disroot.org>
2024-12-25 12:06:37 -0500

    tune: increase max depths for search

--- src/search.rs
+++ src/search.rs
@@ -122,8 +122,8 @@ impl Default for SearchConfig {
     fn default() -> Self {
         SearchConfig {
             alpha_beta_on: true,
-            depth: 10,
-            qdepth: 4,
+            depth: 16,
+            qdepth: 6,
             enable_trans_table: true,
             transposition_size: 24,
         }
